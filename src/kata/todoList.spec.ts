bc8310e209ea633524f82543cb98c3e061c7a7e3
ThomasGil92 <tgil849@gmail.com>
2024-04-17 10:25:47 +0200

    kata to finish

--- src/kata/todoList.spec.ts
+++ src/kata/todoList.spec.ts
@@ -6,7 +6,63 @@ Chaque tâche peut être soit :
 */
 
 // Niveau 1 : pouvoir ajouter une todo dans le système.
+// Niveau 2 : Je souhaite pouvoir supprimer une todo si et seulement si elle n'est pas terminée
+// Niveau 3 : je souhaite catégoriser les todos qui traitent de la même catégorie, et les lister groupés par catégorie
+// Niveau 4 : Je souhaite enregister les todos dans une base de données de sorte à les garder si crash il y a
+// Niveau 5 : ne pas pouvoir ajouter un todo après 20h le soir.
+import { todoManagerFactory } from "./todoList.ts";
 
-describe('Todo-List Management', () => {
+describe("Todo-List Management", () => {
+  let todoManager: ReturnType<typeof todoManagerFactory>;
 
+  beforeEach(() => {
+    todoManager = todoManagerFactory();
+  });
+
+  it("should add a todo to todos", () => {
+    todoManager.addTodo({ name: "first todo", status: "To-Do" });
+    expect(todoManager.getTodos()).toStrictEqual([
+      { name: "first todo", status: "To-Do" },
+    ]);
+  });
+
+  it("should delete a non-terminated todo", () => {
+    todoManager.addTodo({ name: "a todo", status: "In Progress" });
+    todoManager.deleteTodo("a todo");
+    expect(todoManager.getTodos()).toStrictEqual([]);
+  });
+
+  it("should not be possible to delete a todo if it's done", () => {
+    todoManager.addTodo({ name: "a todo", status: "Done" });
+    todoManager.deleteTodo("a todo");
+    expect(todoManager.getTodos()).toStrictEqual([
+      { name: "a todo", status: "Done" },
+    ]);
+  });
+
+  it("should categorise todos", () => {
+    todoManager.addTodo({ name: "a todo", status: "Done", category: "home" });
+    todoManager.addTodo({
+      name: "b todo",
+      status: "In Progress",
+      category: "course",
+    });
+    todoManager.addTodo({ name: "c todo", status: "Done", category: "home" });
+    todoManager.classify();
+    // expect(todoManager.getTodos()).toStrictEqual([
+    //   {
+    //     home: [
+    //       { name: "a todo", status: "Done", category: "home" },
+    //       { name: "c todo", status: "Done", category: "home" },
+    //     ],
+    //     course: [
+    //       {
+    //         name: "b todo",
+    //         status: "In Progress",
+    //         category: "course",
+    //       },
+    //     ],
+    //   },
+    // ]);
+  });
 });
--- src/kata/todoList.ts
+++ src/kata/todoList.ts
@@ -1,2 +1,21 @@
-export class TodoList {
+interface Todo {
+  name: string;
+  status: "To-Do" | "In Progress" | "Done";
+  category?: string;
 }
+
+export const todoManagerFactory = () => {
+  const todos: Record<string, Todo> = {};
+  return {
+    getTodos: () => Object.values(todos),
+    addTodo: (item: Todo) => {
+      todos[item.name] = item;
+    },
+    deleteTodo: (todoName: Todo["name"]) => {
+      if (todos[todoName].status !== "Done") delete todos[todoName];
+    },
+    classify:()=>{
+        
+    }
+  };
+};
